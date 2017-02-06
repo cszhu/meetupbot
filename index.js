4a04054e9c7ae3d64d8b9b4720cb963a05fb965f
Christina Zhu <cszhu@ucdavis.edu>
2017-02-05 20:43:05 -0800

    lat long

--- index.js
+++ index.js
@@ -53,8 +53,8 @@ app.post('/webhook/', function (req, res) {
           console.log("1------1-1-11-1--1-----------")
           console.log(response);
           console.log(response.body)
-          console.log(response.body.results)
-          console.log(response.body.results[0])
+          console.log(response.body[0].results)
+          console.log(response.body[0].results[0])
           console.log(response.body.results[0].geometry)
 
 
