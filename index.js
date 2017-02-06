b35efb3675489a3d30368cf74bb62f19d2245dfc
Christina Zhu <cszhu@ucdavis.edu>
2017-02-06 02:08:36 -0800

    display meetup

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.post('/webhook/', function (req, res) {
     let sender = event.sender.id
     var latitude;
     var longitude;
-    sendTextMessage(sender, "Welcome to the Unofficial Meetup Messenger Bot! To begin, please enter a zip code where you would like to find some Meetups.");
+    // sendTextMessage(sender, "Welcome to the Unofficial Meetup Messenger Bot! To begin, please enter a zip code where you would like to find some Meetups.");
     if (event.message && event.message.text) {
       let text = event.message.text
       if (checkNumber(text) && text.length === 5) {
